dd23c6dde03ba4176220901a0606d7217bbe8720
ysalgueror <135550264+ysalgueror@users.noreply.github.com>
2025-08-27 22:46:44 -0600

    FEAT:COMMIT INICIAL

---
+++ src/main/java/com/grupo2/T4/T4Application.java
@@ -0,0 +1,13 @@
+package com.grupo2.T4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class T4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(T4Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/grupo2/T4/T4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grupo2.T4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class T4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
